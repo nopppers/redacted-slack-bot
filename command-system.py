bcd8266dc9da1536644539c1625cc3fef8aa10ab
Jason Murray <m.jason@digipen.edu>
2017-05-01 16:41:06 -0700

    Change CommandSystem to a different structure

--- command-system.py
+++ command-system.py
@@ -1,35 +1,45 @@
 import bisect
 
+# A handler only takes input and turns it into arguments to a python function
+
 class ResponseSystem(object):
     def __init__(self):
-        self.handlers = []
+        self.handlers = [] # Sorted list of (priority, handlerFunction)
 
-    def add_handler(self, handler):
-        bisect.insort_right(handler)
+    # Adds a handler with the given priority.
+    # 0 means the handler will be tried first.
+    # If multiple handlers are added with the same priority, the most recently added takes precedence.
+    def add_handler(self, priority, handlerFunc):
+        bisect.insort_left(self.handlers, (priority, handlerFunc))
 
+    # Tries every handler in priority order, from 0 to infinity.
+    # Returns which handlers handled the message, and whether or not the message was consumed.
+    # A consumed message will not be passed on to subsequent handlers.
     def handle(self, messageText):
-        for handler in self.handlers:
-            if handler.can_handle_message(messageText):
-                # Only break if the handler consumes the message
-                if handler.handle_message(messageText):
-                    break;
+        result = ResponseResult()
+
+        # This loop returns early if one of the handlers consumed the message
+        for priority, handler in self.handlers:
+            handlerResult = handler(messageText)
+
+            if handlerResult.handled:
+                result.handled.append(handler)
+            if handlerResult.consumed:
+                result.consumed = True
+                return result
+
+        # Consumed is false
+        return result
 
 
-class CommandHandler(object):
-    def __init__(self, priority):
-        self.priority = priority
-
-    def __int__(self):
-        return self.priority
-
-    def __lt__(self, other):
-        return self.priority < other.priority
-
-# A handler can take input as either a string or as python arguments
-# A handler has a name
-# Handlers can access other handlers' functions
+class ResponseResult(object):
+    def __init__(self):
+        self.handled = []
+        self.consumed = False
 
 
-# Or...
-# A handler only takes input and turns it into arguments to a python function
-#
+class HandlerResult(object):
+    def __init__(self, handled = False, consumed = False):
+        self.handled = handled
+        self.consumed = consumed
+
